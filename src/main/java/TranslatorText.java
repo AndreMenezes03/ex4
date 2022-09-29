84cfcc6baac4a494a15f7e3607079db339e89e7d
André Menezes <ndresende@gmail.com>
2022-09-29 17:47:36 -0300

    Initial commit

---
+++ src/main/java/TranslatorText.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+
+import com.google.gson.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class TranslatorText {
+    private static String key = "f932e0f0af084349bde2d605b36cf25c";
+    
+    // location, also known as region.
+   // required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+    private static String location = "brazilsouth";
+
+
+    // Instantiates the OkHttpClient.
+    OkHttpClient client = new OkHttpClient();
+
+    // This function performs a POST request.
+    public String Post() throws IOException {
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType,
+                "[{\"Text\": \"Eu adoraria ter acesso ao serviço da OpenAI do Azure, infelizmente não está disponível\"}]");
+        Request request = new Request.Builder()
+                .url("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=pt-br&to=es&to=en&to=fr&to=it")
+                .post(body)
+                .addHeader("Ocp-Apim-Subscription-Key", key)
+                // location required if you're using a multi-service or regional (not global) resource. 
+                .addHeader("Ocp-Apim-Subscription-Region", location) 
+                .addHeader("Content-type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    // This function prettifies the json response.
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main(String[] args) {
+        try {
+            TranslatorText translateRequest = new TranslatorText();
+            String response = translateRequest.Post();
+            System.out.println(prettify(response));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
